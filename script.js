5ff004767afd147a498f20d7307712419c0240a0
Rahul Kumar Yadav <ry573870@gmail.com>
2025-12-11 12:50:21 +0530

    loader complete

---
+++ script.js
@@ -0,0 +1,43 @@
+let tl = gsap.timeline();
+tl.from(".line h1", {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+tl.from("#line1-part1", {
+  opacity: 0,
+  onStart: function () {
+    let h5timer = document.querySelector("#line1-part1 h5");
+    let grow = 0;
+    let timer = setInterval(function () {
+      h5timer.innerHTML = grow;
+      if (grow >= 100) {
+        clearInterval(timer);
+      }
+      grow++;
+    }, 33);
+  },
+});
+
+tl.to(".line h2",{
+    animationName:"anime",
+    opacity:1
+})
+
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.2,
+  delay: 4,
+});
+
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    duration:0.5,
+    ease:Power4
+})
+tl.to("#loader",{
+    display:"none"
+})
